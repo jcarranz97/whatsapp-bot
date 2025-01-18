009b19aecf1d0530e12ebde217c933d9807aa1b4
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2025-01-18 16:19:51 -0600

    Enable ruff and run as an action

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from typing import Optional
+#!/usr/bin/python3
+"""Main module for the FastAPI application."""
 
 from fastapi import FastAPI
 
@@ -6,9 +7,22 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict:
+    """Return a simple message.
+
+    Returns:
+        dict: A simple message.
+
+    """
     return {"message": "Hello World"}
 
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: int, q: str | None = None) -> dict:
+    """Return the item_id and query parameter.
+
+    Returns:
+        dict: The item_id and query parameter.
+
+    """
     return {"item_id": item_id, "q": q}
